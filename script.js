41e65b0ecf5a3fdd691a679ee5414d0b9c1e8f20
Muhammad <m.shaafan@outlook.de>
2025-04-01 08:39:28 +0200

    alle Bilder

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-const bild = document.createElement("img");
-bild.src = "img/img1.jpg";
-bild.alt = "Das Bild";
-bild.id = "bild";
-bild.alt = "anklickbares Bild"
+const bildQuellen = ["img/img1.jpg",
+    "img/img2.jpg",
+    "img/img3.jpg",
+    "img/img4.jpg",
+    "img/img5.jpg",
+    "img/img6.jpg",
+    "img/img7.jpg",
+    "img/img8.jpg",
+    "img/img9.jpg",
+    "img/img10.jpg",
+    "img/img11.jpg",
+    "img/img12.jpg",
+];
 
+const container = document.getElementById("fotosContainer")
 
-const container = document.getElementById("fotosContainer");
-container.appendChild(bild);
+bildQuellen.forEach((bildQuellen) => {
+    const bild = document.createElement("img");
+    bild.src = bildQuellen;
+    bild.classList.add("albumBild");
+    bild.addEventListener("click", function (event) {
+        event.stopPropagation();
+        bild.classList.toggle("groß");
+    });
 
-bild.addEventListener("click", function(){
-    bild.classList.toggle("groß")
+    container.appendChild(bild)
+
 });
+
+
+document.body.addEventListener("click", function () {
+    const alleBilder = document.querySelectorAll(".albumBild.groß"); 
+    alleBilder.forEach((bild) => bild.classList.remove("groß")); 
+});
+
+
 
